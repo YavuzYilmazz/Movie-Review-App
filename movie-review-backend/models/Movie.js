47b1ade0e02692dd166c014d4985307728b48dc4
Yavuz Yilmaz <74655562+YavuzYilmazz@users.noreply.github.com>
2024-02-12 12:07:01 +0300

    fix(backend): remove versionKey for mongoDb

--- movie-review-backend/models/Movie.js
+++ movie-review-backend/models/Movie.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const reviewSchema = require("./Review").schema;
 
-const movieSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  releaseDate: Date,
-  averageRating: { type: Number, default: 0 },
-  reviews: [reviewSchema],
-});
+const movieSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    releaseDate: Date,
+    averageRating: { type: Number, default: 0 },
+    reviews: [reviewSchema],
+  },
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("Movie", movieSchema);
--- movie-review-backend/models/Review.js
+++ movie-review-backend/models/Review.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema({
-  movie: { type: Schema.Types.ObjectId, ref: "Movie" },
-  body: String,
-  rating: Number,
-  date: Date,
-});
+const reviewSchema = new Schema(
+  {
+    movie: { type: Schema.Types.ObjectId, ref: "Movie" },
+    body: String,
+    rating: Number,
+    date: Date,
+  },
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("Review", reviewSchema);
